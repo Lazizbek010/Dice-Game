c0592426ed6ed8d69bda9a692dafdac45483b259
isaqov1110 <lazizbekisaqov19981110@gmail.com>
2022-03-03 22:57:37 +0500

    end

--- script.js
+++ script.js
@@ -13,37 +13,43 @@ let dice = document.querySelector('.dice')
 dice.style.display = 'none'
 
 // CHANGES
-let currentScore = 0
+let currentScore = 0    
 let activePlayer = 0
 let score = [0, 0]
-
+let gameOver = false
 // EVENTS
 rollBtn.addEventListener('click', rollDice)
 holdBtn.addEventListener('click', hold)
 newBtn.addEventListener('click', reset)
 // FUNCTIONS
 function rollDice() {
-    dice.style.display = 'block'
-    let randomNumber = Math.floor(Math.random() * 6  + 1)
-    dice.src = `dice-${randomNumber}.png `  
+    if(!gameOver){
+        dice.style.display = 'block'
+        let randomNumber = Math.floor(Math.random() * 6  + 1)
+        dice.src = `dice-${randomNumber}.png `  
 
-    if(randomNumber != 1){
-        currentScore += randomNumber
-        document.querySelector(`#current--${activePlayer}`).textContent = currentScore
-    } else{
-        changePlayer()
+        if(randomNumber != 1){
+            currentScore += randomNumber
+            document.querySelector(`#current--${activePlayer}`).textContent = currentScore
+        } else{
+            changePlayer()
+        }
     }
 }
-
 function hold(){
-    score[activePlayer] += currentScore
-    document.querySelector(`#score--${activePlayer}`).textContent = score[activePlayer]
-    changePlayer()
-    if(scorePlayer1 > 20){
-        scorePlayer1.classList.add('player--winner') 
-    } 
+    if(!gameOver){
+        score[activePlayer] += currentScore
+        document.querySelector(`#score--${activePlayer}`).textContent = score[activePlayer]
+        if(score[activePlayer] >= 20){
+            gameOver = true
+            document.querySelector('.player--0').classList.remove('player--active')
+            document.querySelector('.player--1').classList.remove('player--active')
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
+        } else{
+            changePlayer()
+        }
+    }
 }
-
 function changePlayer() {
     document.querySelector(`#current--${activePlayer}`).textContent = "0"
     currentScore = 0
@@ -51,12 +57,19 @@ function changePlayer() {
     document.querySelector('.player--0').classList.toggle('player--active')
     document.querySelector('.player--1').classList.toggle('player--active')
 }
-
 function reset(){
+    currentScore = 0
+    activePlayer = 0
+    score = [0, 0]
+    gameOver = false  
+    document.querySelector(`.player--0`).classList.add('player--active')
+    document.querySelector(`.player--1`).classList.remove('player--active')
+    document.querySelector(`.player--1`).classList.remove('player--winner')
+    document.querySelector(`.player--0`).classList.remove('player--winner')
     dice.style.display = 'none'
     scorePlayer1.textContent = "0"
-    currentPlayer1.textContent = "0"
     scorePlayer2.textContent = "0"
+    currentPlayer1.textContent = "0"
     currentPlayer2.textContent = "0"
 }
 
